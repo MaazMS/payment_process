f393288f256222463991f8e42cfa1c7488334292
maazil3861 <maazshaikh7020@gmail.com>
2021-01-21 23:36:48 +0530

    flask program for paymentProcess

--- code/PaymentProcess.py
+++ code/PaymentProcess.py
@@ -1,23 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from code.validator import ValidateCardInfo
+
 
 app = Flask(__name__)
 
-@app.route('/query1')
+@app.route('/ProcessPayment', methods=['GET'])
 def ProcessPayment():
 
-   creditcardnumber = request.args['creditcardnumber']
-   cardholder = request.args['cardholder']
+   credit_card_number = request.args['creditcardnumber']
+   card_holder = request.args['cardholder']
    expiration_date = request.args['expiration_date']
-   securitycode= request.args['securitycode']
-   check_amount = request.args['check_amount']
+   security_code= request.args['securitycode']
+   amount = request.args['amount']
+
+   credit_card_number_info = ValidateCardInfo.credit_card_number(credit_card_number)
+   card_holder_info = ValidateCardInfo.card_holder(card_holder)
+   expiration_date_info = ValidateCardInfo.expiration_date(expiration_date)
+   security_code_info = ValidateCardInfo.security_code(security_code)
+   amount_info = ValidateCardInfo.check_amount(amount)
 
 
-   return '''<h1>Credit CardNumber: {}</h1>
-   cardholder: {} 
+   return '''Credit CardNumber: {}
+   cardholder: {}   
    ExpirationDate:{} 
    SecurityCode:{} 
-   Amount:{}
-   '''.format(creditcardnumber,cardholder,expiration_date,securitycode,check_amount)
+    Amount:{} '''.format(credit_card_number_info,card_holder_info, expiration_date_info, security_code_info, amount_info )
 
 if __name__ == '__main__':
     app.run(debug= False)
