581d276c93a706ce62a0082eb445afbe60635445
maazil3861 <maazshaikh7020@gmail.com>
2021-01-23 13:12:51 +0530

    receive request from user for payment process

--- app/view/payment_process.py
+++ app/view/payment_process.py
@@ -1,27 +1,12 @@
-from flask import Flask, request
-from app.validator import ValidateCardInfo
-
+from  flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/ProcessPayment', methods=['GET'])
-def ProcessPayment():
-
-   # credit_card_number = request.args['creditcardnumber']
-   # card_holder = request.args['cardholder']
-   # expiration_date = request.args['expiration_date']
-   # security_code= request.args['securitycode']
-   amount = request.args['amount']
-
-   # credit_card_number_info = ValidateCardInfo.credit_card_number(credit_card_number)
-   # card_holder_info = ValidateCardInfo.card_holder(card_holder)
-   # expiration_date_info = ValidateCardInfo.expiration_date(expiration_date)
-   # security_code_info = ValidateCardInfo.security_code(security_code)
-   amount_info = ValidateCardInfo.check_amount(amount)
-
-   return '''
-          Amount:{} '''.format(amount_info)
-
-
-if __name__ == '__main__':
-    app.run(debug= False)
+@app.route('/', methods = ['POST'])
+def process_payment():
+    credit_card_number = request.args.get("creditcardnumber")
+    card_holder = request.args.get("cardholder")
+    expiration_date = request.args.get("expiration_date")
+    security_code = request.args.get("security_code")
+    amount = request.args.get("amount")
+    return jsonify(credit_card_number, card_holder, expiration_date, security_code, amount)
