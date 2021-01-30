db9afe4fba8e334296364282da67ceab7b25ac98
maazil3861 <maazshaikh7020@gmail.com>
2021-01-30 09:23:25 +0530

    define payment process constant

--- app/controller/constants.py
+++ app/controller/constants.py
@@ -2,7 +2,13 @@ from enum import Enum
 
 
 class PaymentGateways(Enum):
+    """ "
+    This class have constant variable which is use to define payment gateway limit and payment gateway name.
+    """
 
     CHEAP_PAYMENT_GATEWAY_LIMIT = 20
     EXPENSIVE_PAYMENT_GATEWAY_LIMIT = 21
     PREMIUM_PAYMENT_GATEWAY_LIMIT = 500
+    CHEAP_PAYMENT_GATEWAY = " Cheap payment gateway"
+    EXPENSIVE_PAYMENT_GATEWAY = "Expensive payment gateway "
+    PREMIUM_PAYMENT_GATEWAY = "Premium payment gateway"
