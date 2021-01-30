97bdf32c261cd70ef4381ec7faf642479384b0b8
maazil3861 <maazshaikh7020@gmail.com>
2021-01-30 09:23:47 +0530

    payment process controller

--- app/controller/payment.py
+++ app/controller/payment.py
@@ -1,28 +1,39 @@
 from base.customer_information.customer import CardDetails
 from pydantic import ValidationError
-from controller.exception import InvalidCardDetailsError, PaymentFailed, \
-    PaymentGatewayNotAvailableError, InvalidAmountError
+from controller.exception import (
+    InvalidCardDetailsError,
+    PaymentFailed,
+    PaymentGatewayNotAvailableError,
+    InvalidAmountError,
+)
 from controller.constants import PaymentGateways
-from base.gateways.payment_gateways import PremiumPaymentGateway, CheapPaymentGateway, ExpensivePaymentGateway
+from base.gateways.payment_gateways import (
+    PremiumPaymentGateway,
+    CheapPaymentGateway,
+    ExpensivePaymentGateway,
+)
 
 
-def process_payment(credit_card_number, card_holder, expiration_date, security_code, amount):
+def process_payment(
+    credit_card_number, card_holder, expiration_date, security_code, amount
+):
     """
-
     :param credit_card_number:
     :param card_holder:
     :param expiration_date:
     :param security_code:
     :param amount:
-    :return:
+    :return: flag and string
+     flag : Flag show the status of payment which are true means payment successful and false means payment failed
+    string : The string are show that payment successful with payment gateway name
     """
+
     try:
         card_details = CardDetails(
             card_number=credit_card_number,
             customer_name=card_holder,
             exp_date=expiration_date,
             cvv=security_code,
-
         )
 
     except ValidationError as e:
@@ -37,23 +48,24 @@ def process_payment(credit_card_number, card_holder, expiration_date, security_c
         try:
             cheap_payment_gateway = CheapPaymentGateway()
             cheap_payment_gateway.create_transaction(**target_card_details)
-
+            return True, PaymentGateways.CHEAP_PAYMENT_GATEWAY
         except Exception as e:
             # TODO log(e)
             raise PaymentFailed("Transaction  failed")
 
     elif (int(amount) >= PaymentGateways.EXPENSIVE_PAYMENT_GATEWAY_LIMIT.value) and (
-            int(amount) <= PaymentGateways.PREMIUM_PAYMENT_GATEWAY_LIMIT.value):
+        int(amount) <= PaymentGateways.PREMIUM_PAYMENT_GATEWAY_LIMIT.value
+    ):
         try:
             expensive_payment_gateway = ExpensivePaymentGateway()
             expensive_payment_gateway.create_transaction()
-
+            return True, PaymentGateways.EXPENSIVE_PAYMENT_GATEWAY
         except PaymentGatewayNotAvailableError as e:
             # TODO log(e)
             try:
                 cheap_payment_gateway = CheapPaymentGateway()
                 cheap_payment_gateway.create_transaction(**target_card_details)
-
+                return True, PaymentGateways.CHEAP_PAYMENT_GATEWAY
             except Exception as e:
                 # TODO: log(e)
                 raise PaymentFailed("Transaction failed")
@@ -64,8 +76,9 @@ def process_payment(credit_card_number, card_holder, expiration_date, security_c
             try:
                 premium_payment_gateway = PremiumPaymentGateway()
                 premium_payment_gateway.create_transaction(**target_card_details)
-
+                return True, PaymentGateways.PREMIUM_PAYMENT_GATEWAY
             except Exception as e:
                 # TODO log(e)
                 if retry == 3:
                     raise PaymentFailed("Transaction failed")
+            retry += 1
